85b5d21f6877374a00327d72563f3594a0020055
byun <markorea9708@gmail.com>
2023-08-17 01:04:39 +0900

    simple rest api

--- main.go
+++ main.go
@@ -13,19 +13,17 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "go_Micro", log.LstdFlags)
-	hh := handlers.New_hello(l)
-	gh := handlers.New_Goodbye(l)
+	ph := handlers.NewProducts(l)
 
 	sm := http.NewServeMux()
-	sm.Handle("/", hh)
-	sm.Handle("/goodbye", gh)
+	sm.Handle("/", ph)
 
 	s := &http.Server{
 		Addr:         ":8080",
 		Handler:      sm,
 		IdleTimeout:  120 * time.Second,
-		ReadTimeout:  1 * time.Second,
-		WriteTimeout: 1 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
 	}
 
 	go func() {
